dec177d8abadd35bc7a39910fb5953f3f0363a1c
DANILO ALMEIDA DAMASCENO <71226047+DaniloADamasceno@users.noreply.github.com>
2022-07-24 17:16:16 -0300

    :rocket: Exercício Try e Catch

---
+++ Tratamento de Exceções/Hotel - Exceções forma BOA/Hotel - Exceções forma BOA/src/model/entities/Reservation.java
@@ -0,0 +1,104 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import model.Exceptions.DomainException;
+
+//import javax.lang.model.util.SimpleAnnotationValueVisitor14;
+
+public class Reservation {
+
+  private Integer roomNumber;
+  private Date checkin;
+  private Date checkout;
+
+  private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+  //---------------
+  // Constructors
+  //---------------
+
+  public Reservation() {}
+
+  public Reservation(Integer roomNumber, Date checkin, Date checkout) throws DomainException {
+    if (!checkout.after(checkin)) {
+      throw new DomainException("Checkout date must be after checkin date");
+    }
+
+    this.roomNumber = roomNumber;
+    this.checkin = checkin;
+    this.checkout = checkout;
+  }
+
+  //---------------
+  // Getter/Setter
+  //---------------
+
+  public Integer getRoomNumber() {
+    return roomNumber;
+  }
+
+  public void setRoomNumber(Integer roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  public Date getCheckin() {
+    return checkin;
+  }
+
+  public Date getCheckout() {
+    return checkout;
+  }
+
+  //---------------
+  // Methods
+  //---------------
+
+  public long duration() {
+    long diff = checkout.getTime() - checkin.getTime();
+    // para converter o tempo em milissegundos utilizando a classe TIME_UNIT
+    return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    //
+    // se não utilizando a classe TIME_UNIT, bastaria fazer:
+    // return (long) (diff / (1000 * 60 * 60 * 24));
+  }
+
+  /**
+   * @param checkin
+   * @param checkout
+   */
+  public void updateDates(Date checkin, Date checkout) throws DomainException {
+    Date now = new Date();
+    if (checkin.before(now) || checkout.before(now)) {
+      throw new DomainException(
+        "Reservation dates for update must be future dates"
+      );
+    }
+    if (!checkout.after(checkin)) {
+      throw new DomainException("Checkout date must be after checkin date");
+    }
+    this.checkin = checkin;
+    this.checkout = checkout;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "--->>  Hotel transilvânia  <<---" +
+      "\n" +
+      "Room: " +
+      roomNumber +
+      "\n" +
+      "Check-in: " +
+      sdf.format(checkin) +
+      "\n" +
+      "Check-out: " +
+      sdf.format(checkout) +
+      "\n" +
+      "Duration: " +
+      duration() +
+      " nights"
+    );
+  }
+}
