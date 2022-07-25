103f3aa07a55411429ed11d44a861a279b25b911
DaniloADamasceno <danilodamasceno@seatecnologia.com.br>
2022-07-25 17:00:14 -0300

    :truck: Renomear App.java -->> Chess_Game

--- Sistema de Jogo de XADREZ/XADREZ/src/application/Chess_Game.java
+++ Sistema de Jogo de XADREZ/XADREZ/src/application/Chess_Game.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
 import boardGame.Board;
 import boardGame.Position;
 
-public class App {
+public class Chess_Game {
 
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
     Locale.setDefault(Locale.US);
 
-Board board = new Board(8, 8);
+    Board board = new Board(8, 8);
     System.out.println(board);
     in.close();
   }
 }
+
