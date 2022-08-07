4243fc2405d028c44e3f2f6bdef740cd6ccf7cc1
DaniloADamasceno <danilo.engauto@gmail.com>
2022-08-07 11:36:31 -0300

    :sparkles: :zap: Melhoria de Tabuleiro | Add Peças

--- Sistema de Jogo de XADREZ/XADREZ/src/application/User_Interface.java
+++ Sistema de Jogo de XADREZ/XADREZ/src/application/User_Interface.java
@@ -1,8 +1,36 @@
 package application;
 
 import chess.Chess_Piece;
+import chess.Color;
 
 public class User_Interface {
+
+  // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+  //-----------------------
+  //      TEXT COLORS 
+  //-----------------------
+  public static final String ANSI_RESET = "\u001B[0m";    // Reset a Cor
+  public static final String ANSI_BLACK = "\u001B[30m";   // Preto
+  public static final String ANSI_RED = "\u001B[31m";     // Vermelho
+  public static final String ANSI_GREEN = "\u001B[32m";   // Verde
+  public static final String ANSI_YELLOW = "\u001B[33m";  // Amarelo
+  public static final String ANSI_BLUE = "\u001B[34m";    // Azul
+  public static final String ANSI_PURPLE = "\u001B[35m";  // Roxo
+  public static final String ANSI_CYAN = "\u001B[36m";    // Ciano / Azul Claro
+  public static final String ANSI_WHITE = "\u001B[37m"; // Branco
+
+  //----------------------------
+  //    BACKGROUND COLORSSTYLES
+  //----------------------------
+  public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";    // Preto
+  public static final String ANSI_RED_BACKGROUND = "\u001B[41m";      // Vermelho
+  public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";    // Verde
+  public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";   // Amarelo
+  public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";     // Azul
+  public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";   // Roxo
+  public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";     // Ciano / Azul Claro
+  public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";    // Branco
 
   //----------------------
   //     Methods
@@ -16,19 +44,26 @@ public class User_Interface {
         printPiece(pieces[i][j]);
       }
       System.out.println(); // Quebra de linha
+
     }
-    System.out.println("* A B C D E F G H");
+    System.out.println("*                ");
+    System.out.println("* a b c d e f g h");
   }
 
-
-  // -------------------
-  //*  Melhorar a Visualização do tabuleiro   || Não esta aparecendo a Letra Da peça no tabuleiro
-  // -------------------
   private static void printPiece(Chess_Piece piece) {
     if (piece == null) {
-      System.out.print("- ");
-    } else {
-      System.out.print("" + piece.toString().charAt(0) + " ");
+      System.out.print("-");
+    } 
+      else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+                //   System.out.print("" + piece.toString().charAt(0) + " ");
+            }
+        }
+
+System.out.print(" ");  
     }
   }
-}
--- Sistema de Jogo de XADREZ/XADREZ/src/chess/Chess_Match.java
+++ Sistema de Jogo de XADREZ/XADREZ/src/chess/Chess_Match.java
@@ -2,7 +2,10 @@ package chess;
 
 import boardGame.Board;
 import boardGame.Position;
+import chessPieces.Bishop;
 import chessPieces.King;
+import chessPieces.Knight;
+import chessPieces.Pawn;
 import chessPieces.Queen;
 import chessPieces.Rook;
 
@@ -38,16 +41,58 @@ public class Chess_Match {
   }
 
   private void initialSetup() {
-    placeNewPiece('b', 6, new Rook(board, Color.WHITE)); // Torre branca #01
-    placeNewPiece('b', 1, new Rook(board, Color.WHITE)); // Torre branca #02
 
-    placeNewPiece('a', 6, new Rook(board, Color.BLACK)); // Torre preta #01
-    placeNewPiece('a', 2, new Rook(board, Color.BLACK)); // Torre preta #02
+    //---------------------------------------
+    //   ROOKS / TORRES ( Brancas e Pretas )
+    //---------------------------------------
+    placeNewPiece('a', 1, new Rook(board, Color.WHITE)); // Torre branca #01
+    placeNewPiece('h', 1, new Rook(board, Color.WHITE)); // Torre branca #02
 
-    placeNewPiece('c', 3, new King(board, Color.WHITE));
-    placeNewPiece('d', 4, new King(board, Color.BLACK));
+    placeNewPiece('h', 8, new Rook(board, Color.BLACK)); // Torre preta #01
+    placeNewPiece('a', 8, new Rook(board, Color.BLACK)); // Torre preta #02
 
-    placeNewPiece('e', 5, new Queen(board, Color.BLACK));
-    placeNewPiece('f', 6, new Queen(board, Color.WHITE));
+    //---------------------------------------
+    //   KNIGHTS / CAVALO ( Brancas e Pretas )
+    //---------------------------------------
+    placeNewPiece('b', 1, new Knight(board, Color.WHITE)); // Cavaleiro branco #01
+    placeNewPiece('g', 1, new Knight(board, Color.WHITE)); // Cavaleiro branco #02
+
+    placeNewPiece('g', 8, new Knight(board, Color.BLACK)); // Cavaleiro preta #01
+    placeNewPiece('b', 8, new Knight(board, Color.BLACK)); // Cavaleiro preta #02
+
+    //---------------------------------------
+    //   BISHOPS / BISPO ( Brancas e Pretas )
+    //---------------------------------------
+    placeNewPiece('c', 1, new Bishop(board, Color.WHITE)); // Bispo branco #01
+    placeNewPiece('f', 1, new Bishop(board, Color.WHITE)); // Bispo branco #02
+
+    placeNewPiece('f', 8, new Bishop(board, Color.BLACK)); // Bispo preta #01
+    placeNewPiece('c', 8, new Bishop(board, Color.BLACK)); // Bispo preta #02
+
+    //---------------------------------------
+    //   QUEENS / RAINHA ( Brancas e Pretas )
+    //---------------------------------------
+    placeNewPiece('d', 1, new Queen(board, Color.WHITE)); // Rainha branca #01
+    placeNewPiece('d', 8, new Queen(board, Color.BLACK)); // Rainha preta #01
+
+    //---------------------------------------
+    //   KINGS / REI ( Brancas e Pretas )
+    //---------------------------------------
+    placeNewPiece('e', 1, new King(board, Color.WHITE)); // Rei branco #01
+    placeNewPiece('e', 8, new King(board, Color.BLACK)); // Rei preta #01
+
+    //---------------------------------------
+    //   PAWNS / PEÕES ( Brancas e Pretas )
+    //---------------------------------------
+    // for (int i = 0; i < 8; i++) {
+    //   placeNewPiece('a', 2 + i, new Pawn(board, Color.WHITE)); // Peões brancos #01
+    //   placeNewPiece('h', 2 + i, new Pawn(board, Color.BLACK)); // Peões pretos #01
+    // }
+
+
+
+    
+    
+ 
   }
 }
