d88aacad3f2335eb5e78cbc017406809c0713b20
DaniloADamasceno <danilo.engauto@gmail.com>
2022-09-01 18:55:22 -0300

    :tada: Pasta de locação de veiculos

---
+++ Locadora de Veiculos - Interface/Locadora de Veiculos/src/Locadora_Veiculos.java
@@ -0,0 +1,34 @@
+public class Locadora_Veiculos {
+
+  public static void main(String[] args) throws Exception {
+    /*
+    Uma locadora brasileira de carros cobra um valor por hora para locações de até 12 horas. 
+    Porém, se a duração da locação ultrapassar 12 horas, a locação será cobrada com base em um valor diário.
+    Além do valor da locação, é acrescido no preço o valor do imposto conforme regras do país que, no caso do Brasil, é 20% 
+    para valores até 100.00, ou 15% para valores acima de 100.00. 
+    Fazer um programa que lê os dados da locação (modelo do carro, instante inicial e final da locação), 
+    bem como o valor por hora e o valor diário de locação.
+    O programa deve então gerar a nota de pagamento (contendo valor da locação, valor do imposto e valor total do pagamento)
+    e informar os dados na tela. Veja os exemplos.
+    
+    ----------------------------------------------------- | ----------------------------------------------------------------
+                    Exemplo 1:                            |                               Exemplo 2:
+    Car model:  Honda Civic                               |                 Car model:  Honda Civic II
+    Pickup (dd/MM/yyyy HH:mm): 25/06/2022 10:32           |                 Pickup (dd/MM/yyyy HH:mm): 20/08/2022 10:31
+    Return (dd/MM/yyyy HH:mm): 25/06/2022 14:42           |                 Return (dd/MM/yyyy HH:mm): 22/08/2022 11:47
+    Price per hour: 10.00                                 |                 Price per hour: 10.00
+    Price per day: 130.00                                 |                 Price per day: 130.00
+                                                          |
+    INVOICE:                                              |                  INVOICE:
+    Basic payment: 50.00                                  |                  Basic payment: 390.00
+    Tax: 10.00                                            |                  Tax: 58.50
+    Total payment: 60.00                                  |                  Total payment: 448.50
+    ----------------------------------------------------- | ----------------------------------------------------------------
+    */
+    
+    
+    
+    
+
+  }
+}
